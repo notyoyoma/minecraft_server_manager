059ff263bd74887cc661f829ddafeb1a35eb81fd
Marty Naselli <marty.naselli@gmail.com>
2016-03-08 13:48:12 -0500

    logins working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'app/index'
+
   devise_for :users
   devise_scope :user do
     authenticated :user do
-      root "home#index", as: :authenticated_root
+      root "app#index", as: :authenticated_root
     end
 
     unauthenticated do
